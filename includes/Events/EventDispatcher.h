0de12d3267dd3f561f6e67927f922be3fdd7c2d3
acoezard <axel.coezard@gmail.com>
2023-03-07 22:17:30 +0100

    add support for event dispatching

---
+++ includes/Events/EventDispatcher.h
@@ -0,0 +1,37 @@
+/*
+ * File: EventHandler.h
+ * Author: Axel Coezard (hello@axelcoezard.com)
+ * -----
+ * File Created: Monday, 6th March 2023 4:47:21 pm
+ * Last Modified: Tuesday, 7th March 2023 9:56:33 pm
+ * Modified By: Axel Coezard (hello@axelcoezard.com>)
+ */
+
+#pragma once
+
+#include <string>
+#include <string_view>
+#include <map>
+
+#include "Events/Event.h"
+#include "Events/EventListener.h"
+
+struct EventDispatcher {
+	private:
+		static EventDispatcher* m_instance;
+
+		std::map<std::string, EventListener*> m_listeners;
+
+	public:
+		EventDispatcher(void);
+		~EventDispatcher(void);
+
+		EventDispatcher(EventDispatcher&) = delete;
+		void operator=(const EventDispatcher&) = delete;
+
+		void RegisterListener(std::string_view eventName, EventListener* eventListener);
+		void DispatchEvent(std::string_view eventName, Event* event);
+
+		static EventDispatcher* Get(void);
+		static void Destroy(void);
+};
