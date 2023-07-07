3582dcef8b020e4dfc32872158c5e5a127c37a79
acoezard <axel.coezard@gmail.com>
2023-07-07 21:29:19 +0200

    add installation setup script

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo apt install vulkan-tools -y
+sudo apt install libvulkan-dev -y
+sudo apt install vulkan-validationlayers-dev spirv-tools -y
+
+sudo apt install libglfw3-dev -y
+sudo apt install libglm-dev -y
