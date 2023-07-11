58baed3640e8cb183be1590bfc34988fe7c2fd05
Axel Coézard <axel.coezard@gmail.com>
2023-07-11 13:35:36 +0200

    updated setup linux script

--- setup-linux.sh
+++ setup-linux.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Install all required tools using apt
+sudo apt install cmake -y
 sudo apt install vulkan-tools -y
 sudo apt install libvulkan-dev -y
 sudo apt install vulkan-validationlayers-dev spirv-tools -y
