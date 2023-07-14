34f5252c176873022878b9d4115ae0390ec761e6
acoezard <axel.coezard@gmail.com>
2023-07-14 17:23:53 +0200

    add z axis to vertices

--- BetterThanNothing/assets/main.vert
+++ BetterThanNothing/assets/main.vert
@@ -6,7 +6,7 @@ layout(binding = 0) uniform UniformBufferObject {
 	mat4 m_Projection;
 } ubo;
 
-layout(location = 0) in vec2 inPosition;
+layout(location = 0) in vec3 inPosition;
 layout(location = 1) in vec3 inColor;
 layout(location = 2) in vec2 inTexCoord;
 
@@ -14,7 +14,7 @@ layout(location = 0) out vec3 fragColor;
 layout(location = 1) out vec2 fragTexCoord;
 
 void main() {
-	gl_Position = ubo.m_Projection * ubo.m_View * ubo.m_Model * vec4(inPosition, 0.0, 1.0);
+	gl_Position = ubo.m_Projection * ubo.m_View * ubo.m_Model * vec4(inPosition, 1.0);
 	fragColor = inColor;
 	fragTexCoord = inTexCoord;
 }
--- BetterThanNothing/sources/Engine/CVertex.hpp
+++ BetterThanNothing/sources/Engine/CVertex.hpp
@@ -4,7 +4,7 @@ namespace BetterThanNothing
 {
 	struct Vertex
 	{
-		glm::vec2	m_Position;
+		glm::vec3	m_Position;
 		glm::vec3	m_Color;
 		glm::vec2	m_TextureCoordinates;
 
@@ -20,7 +20,7 @@ namespace BetterThanNothing
 			std::array<VkVertexInputAttributeDescription, 3> attributeDescriptions{};
 			attributeDescriptions[0].binding = 0;
 			attributeDescriptions[0].location = 0;
-			attributeDescriptions[0].format = VK_FORMAT_R32G32_SFLOAT;
+			attributeDescriptions[0].format = VK_FORMAT_R32G32B32_SFLOAT;
 			attributeDescriptions[0].offset = offsetof(Vertex, m_Position);
 
 			attributeDescriptions[1].binding = 0;
