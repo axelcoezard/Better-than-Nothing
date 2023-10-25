4a2cd122d7a7659dddf795a5c47834baf22baa4c
acoezard <axel.coezard@gmail.com>
2023-10-25 13:35:52 +0200

    add back all libs without submodules

---
+++ ThirdParty/stb/stb_image.cpp
@@ -0,0 +1,17 @@
+/*
+ * File: stb_image.cpp
+ * Author: Axel Coezard (hello@axelcoezard.com)
+ * -----
+ * File Created: Sunday, 26th March 2023 2:37:39 pm
+ * Last Modified: Sunday, 26th March 2023 2:37:40 pm
+ * Modified By: Axel Coezard (hello@axelcoezard.com>)
+ */
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+#define STB_IMAGE_RESIZE_IMPLEMENTATION
+#include "stb_image_resize.h"
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
