b0f5b4f9dd8b7d1c12731ee250e2f2f1db689662
acoezard <axel.coezard@gmail.com>
2023-11-13 11:55:17 +0100

    add libxinerama-dev

--- setup-linux.sh
+++ setup-linux.sh
@@ -6,7 +6,7 @@ sudo apt install vulkan-tools libvulkan-dev vulkan-validationlayers-dev spirv-to
 sudo apt install libglfw3-dev libglm-dev -y
 sudo apt install doxygen doxygen-gui doxygen-doc -y
 sudo apt install wget -y
-sudo apt install libxinerama -y
+sudo apt install libxinerama-dev -y
 
 # Install glslc using the downloaded archive
 wget https://storage.googleapis.com/shaderc/artifacts/prod/graphics_shader_compiler/shaderc/linux/continuous_clang_release/419/20230623-090916/install.tgz
