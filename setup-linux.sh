874833d105f4c2247df56bbff6032095114b2790
acoezard <axel.coezard@gmail.com>
2023-11-13 11:57:41 +0100

    add libxcursor-dev

--- setup-linux.sh
+++ setup-linux.sh
@@ -6,7 +6,9 @@ sudo apt install vulkan-tools libvulkan-dev vulkan-validationlayers-dev spirv-to
 sudo apt install libglfw3-dev libglm-dev -y
 sudo apt install doxygen doxygen-gui doxygen-doc -y
 sudo apt install wget -y
-sudo apt install libxinerama-dev -y
+
+# Insall all required packages for Github actions
+sudo apt install libxinerama-dev libxcursor-dev -y
 
 # Install glslc using the downloaded archive
 wget https://storage.googleapis.com/shaderc/artifacts/prod/graphics_shader_compiler/shaderc/linux/continuous_clang_release/419/20230623-090916/install.tgz
