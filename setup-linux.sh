edf08ff658f22cbf99d4d905756d2dc5f79b0dd1
acoezard <axel.coezard@gmail.com>
2023-11-13 11:59:44 +0100

    added libxi-dev

--- setup-linux.sh
+++ setup-linux.sh
@@ -8,7 +8,7 @@ sudo apt install doxygen doxygen-gui doxygen-doc -y
 sudo apt install wget -y
 
 # Insall all required packages for Github actions
-sudo apt install libxinerama-dev libxcursor-dev -y
+sudo apt install libxinerama-dev libxcursor-dev libxi-dev -y
 
 # Install glslc using the downloaded archive
 wget https://storage.googleapis.com/shaderc/artifacts/prod/graphics_shader_compiler/shaderc/linux/continuous_clang_release/419/20230623-090916/install.tgz
