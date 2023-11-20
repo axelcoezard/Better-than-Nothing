5632c5b1c04fbe1c06bffb3a3363db1227f28be7
acoezard <axel.coezard@gmail.com>
2023-11-20 18:03:14 +0100

    add documentation to Application.hpp

--- Better-than-Nothing/Modules/Core/src/Application.hpp
+++ Better-than-Nothing/Modules/Core/src/Application.hpp
@@ -12,33 +12,98 @@ namespace BetterThanNothing
 	class Scene;
 	class Event;
 
+	/**
+	 * @brief The Application class is the main class of the engine's applications.
+	 */
 	class Application
 	{
 	protected:
-		Window*				m_Window;
-		Device*				m_Device;
-		Renderer*			m_Renderer;
+		/**
+		 * @brief A pointer to the Window
+		*/
+		Window* m_Window;
 
-		ModelPool*			m_ModelPool;
-		TexturePool*		m_TexturePool;
+		/**
+		 * @brief A pointer to the Device
+		*/
+		Device* m_Device;
 
-		std::vector<Scene*>	m_Scenes;
-		u32					m_CurrentSceneId;
+		/**
+		 * @brief A pointer to the Renderer
+		*/
+		Renderer* m_Renderer;
+
+		/**
+		 * @brief A pointer to the ModelPool
+		*/
+		ModelPool* m_ModelPool;
+
+		/**
+		 * @brief A pointer to the TexturePool
+		*/
+		TexturePool* m_TexturePool;
+
+		/**
+		 * @brief The Scenes of the Application
+		*/
+		std::vector<Scene*> m_Scenes;
+
+		/**
+		 * @brief The current Scene of the Application
+		*/
+		u32 m_CurrentSceneId;
 	public:
-							Application(std::string_view title, u32 width, u32 height);
-		virtual				~Application();
+		/**
+		 * @brief Construct a new Application object
+		 * @param title The title of the window
+		 * @param width The width of the window
+		 * @param height The height of the window
+		*/
+		Application(std::string_view title, u32 width, u32 height);
 
-		void				Run();
+		/**
+		 * @brief Destroy the Application object
+		*/
+		virtual ~Application();
 
-		virtual void		OnEnable() = 0;
-		virtual void		OnDisable() = 0;
+		/**
+		 * @brief Run the Application
+		*/
+		void Run();
 
-		void				OnEvent(Event* event);
+		virtual void OnEnable() = 0;
+		virtual void OnDisable() = 0;
 
-		Scene*				CreateScene(const std::string& name);
+		/**
+		 * @brief Called when an Event is received
+		 * @param event A pointer to the Event object received
+		*/
+		void OnEvent(Event* event);
 
-		Window*				GetWindow() { return m_Window; }
-		Device*				GetDevice() { return m_Device; }
-		Renderer*			GetRenderer() { return m_Renderer; }
+		/**
+		 * @brief Create a Scene object and register it to the Application
+		 *
+		 * @param name The name of the Scene
+		 * @return A pointer to the created Scene object
+		 */
+		Scene* CreateScene(const std::string& name);
+
+		/**
+		 * @brief Get the Window object
+		 * @return A pointer to the Window object
+		 */
+		Window* GetWindow() { return m_Window; }
+
+		/**
+		 * @brief Get the Device object
+		 * @return A pointer to the Device object
+		 */
+		Device* GetDevice() { return m_Device; }
+
+		/**
+		 * @brief Get the Renderer object
+		 * @return A pointer to the Renderer object
+		 */
+		Renderer* GetRenderer() { return m_Renderer; }
 	};
 };
