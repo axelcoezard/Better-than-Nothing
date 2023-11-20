49c538ecb7a564d4bb232282cb9d3b38c29aecd2
acoezard <axel.coezard@gmail.com>
2023-11-20 15:56:29 +0100

    changed ouput

--- Better-than-Nothing/Shaders/compile_shaders.sh
+++ Better-than-Nothing/Shaders/compile_shaders.sh
@@ -3,6 +3,6 @@
 for shader in $(find ${1:-$(pwd)} -name "*.vert" -o -name "*.frag");
 do
 	rm -f "$shader.spv"
+	echo "Compiling GLSL shader $shader.spv"
 	glslc $shader -o "$shader.spv"
-	echo "Compiled $shader to $shader.spv"
 done
