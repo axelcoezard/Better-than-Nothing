2ece03e536f077ee676fce1a8beff41b8c47483d
acoezard <axel.coezard@gmail.com>
2023-11-21 14:46:47 +0100

    added more docs

--- Better-than-Nothing/Modules/Core/src/Renderer/GlobalUniforms.hpp
+++ Better-than-Nothing/Modules/Core/src/Renderer/GlobalUniforms.hpp
@@ -2,10 +2,24 @@
 
 namespace BetterThanNothing
 {
+	/**
+	 * @brief The global uniforms
+	 */
 	struct GlobalUniforms
 	{
+		/**
+		 * @brief The model matrix
+		 */
 		alignas(16) glm::mat4	model;
+
+		/**
+		 * @brief The camera view matrix
+		 */
 		alignas(16) glm::mat4	view;
+
+		/**
+		 * @brief The camera projection matrix
+		 */
 		alignas(16) glm::mat4	projection;
 	};
 };
