0fe38282bb54c3c914b8ee9984fb43d8f897a39a
acoezard <axel.coezard@gmail.com>
2023-11-21 15:04:10 +0100

    update docs folder name

--- doxygen-linux.sh
+++ doxygen-linux.sh
@@ -4,20 +4,16 @@
 # Variables
 # ==================================================================================================
 
-destination_folder="Better-than-Nothing/Documentation"
-source_folder="$destination_folder/html"
+destination_folder="Better-than-Nothing/docs"
 
 # ==================================================================================================
 # Script
 # ==================================================================================================
 
-rm -rf $destination_folder
-mkdir $destination_folder
+mkdir -p $destination_folder
+rm -rf $destination_folder/*
 
 doxygen Doxyfile &>/dev/null
 
-mv $source_folder/* $destination_folder
-rm -rf $source_folder
-
 echo -e "Doxygen documentation updated and avalaible here:"
-echo -e "=> \e[1;32mfile://$PWD/$destination_folder/index.html\e[0m"
+echo -e "=> \e[1;32mfile://$PWD/$destination_folder/html/index.html\e[0m"
