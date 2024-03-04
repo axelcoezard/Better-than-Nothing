95d5d7f580fa40b2cb9656e44f47816d5f5ea4b6
¨Axel <axel.coezard@gmail.com>
2024-03-04 09:40:11 +0100

    all fixes

--- Better-than-Nothing/Modules/pch.hpp
+++ Better-than-Nothing/Modules/pch.hpp
@@ -14,11 +14,11 @@
 #define GLM_FORCE_RADIANS
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
 #define GLM_FORCE_DEFAULT_ALIGNED_GENTYPES
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
+#include <glm.hpp>
+#include <gtc/matrix_transform.hpp>
 
 #define GLM_ENABLE_EXPERIMENTAL
-#include <glm/gtx/hash.hpp>
+#include <gtx/hash.hpp>
 
 #include <INIReader.h>
 
